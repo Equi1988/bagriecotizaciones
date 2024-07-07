f305cf0e63eb656d6fa71406eaf7efcb5fd7a412
ezequielriente@gmail.com <ezequielriente@gmail.com>
2024-07-06 22:49:19 -0300

    Array y Objetos

--- java/main.js
+++ java/main.js
@@ -1,70 +1,190 @@
+// Bienvenidos
+
+alert ("¡Bienvenidos a BagRie Cotizaciones!");
 
 // ENTRADA DE DATOS
+
+const nombres = [ "Ezequiel Riente", "Soledad Sanguineti", "Damian Cerezo"];
 let nombreCliente = prompt("Ingrese su Nombre y Apellido: (ESC para salir)");
+    let posArray = nombres.indexOf(nombreCliente);
+    if (posArray >= 0) {
+        alert("El nombre se encuentra en la posición: " + posArray)
+    } else {
+        alert("Error! No existe el nombre ingresado!");
+    } 
+let identificadorDni = prompt ("Ingrese su DNI: ");
 
-while (nombreCliente != "ESC") {
-    // let textoTipoCliente = "Ingrese su Paquete de Servicios:\n\n";
-    // textoTipoCliente += "1-BBVA Premium\n";
-    // textoTipoCliente += "2-BBVA Premiun World\n";
-    // textoTipoCliente += "3-BBVA Advanced\n";
-    // textoTipoCliente += "4-BBVA Standard";
-    // let tipoCliente = parseInt(prompt(textoTipoCliente));
-    let monto = parseInt(prompt("Ingrese el Monto: ($10000 - $1000000"));
-    let textoTasa = "Ingrese Tasa:\n\n";
-    textoTasa += "1-40%\n";
-    textoTasa += "2-60%\n";
-    textoTasa += "3-90%\n";
-    textoTasa += "4-120%";
-    let tasa = parseInt (prompt (textoTasa));
-    let textoPlazo = "Ingrese el Plazo:\n\n";
-    textoPlazo += "1- 30 dias\n";
-    textoPlazo += "2- 60 dias\n";
-    textoPlazo += "3- 90 dias\n";
-    textoPlazo += "4- 120 dias";
-    let plazo = parseInt(prompt(textoPlazo));
+//Metodo Objeto
 
-    // PROCESAMIENTO DE DATOS
-    let plazofijoPuro = calcularPlazoFijoPuro(monto, plazo);
-    let plazofijoTNA =  calcularplazofijoTNA(tasa, plazofijoPuro);
-    let interesapagar = plazofijoTNA;
+nombreCliente = formatearnombreCliente (nombreCliente);
 
-    // SALIDA DE DATOS
-    informarPlazoFjo(nombreCliente, monto, plazo, tasa, interesapagar);
-    nombreCliente = prompt("Ingrese otro Nombre y Apellido: (ESC para salir)");
+function formatearnombreCliente (nombreCliente){
+    return nombreCliente.trim().toUpperCase();
 }
 
-function calcularPlazoFijoPuro(monto, plazo, tasa) {
-    return (monto * plazo * tasa/100 / 365) ;
-}
+while (nombreCliente != "ESC"){
 
-function calcularplazofijoTNA(tasa, plazofijoPuro) {
-    let TNA;
-
-    if (tasa == 1) {
-        TNA = 40 / 100;
-    } else if (tasa == 2) {
-        TNA = 60 / 100;
-    } else if (tasa == 3) {
-        TNA = 90 / 100;
-    } else  {
-        TNA = 120 / 100;
+    if (nombreCliente.toUpperCase() == "ESC") {
+        break;
     }
 
-    return (plazofijoPuro * TNA)/100;
+    let textoSosCliente ="¿Sos Cliente?\n\n";
+    textoSosCliente += "1-Si\n";
+    textoSosCliente += "2-No";
+let cliente = parseInt (prompt (textoSosCliente));
+
+    if (textoSosCliente == 2){
+        continue;
 }
 
-function informarPlazoFjo(nombre, monto, plazo, tasa) {
+    let textoTipoCliente = "Ingrese su Paquete de Servicios:\n\n";
+    textoTipoCliente += "1-Premium\n";
+    textoTipoCliente += "2-Premiun World\n";
+    textoTipoCliente += "3-Advanced\n";
+    textoTipoCliente += "4-Standard";
+let tipoCliente = parseInt(prompt(textoTipoCliente));   
+
+
+
+
+// let textoTipoPlazoFijo ="Ingrese tipo de Plazo Fijo:\n\n";
+//     textoTipoPlazoFijo += "1-Plazo Fijo en pesos\n";
+//     // textoTipoPlazoFijo += "2-Plazo Fijo en dólares (sólo clientes)";
+// let tipoPlazoFijo = parseInt (prompt (textoTipoPlazoFijo));
+
+let monto = parseInt(prompt("Ingrese el Monto:(Minimo $ 1000)")); 
+
+let textoPlazo ="Ingrese tipo de Plazo:\n\n";
+    textoPlazo +="1-30 dias\n";  
+    textoPlazo +="2-60 dias\n";
+    textoPlazo +="3-90 dias\n";
+    textoPlazo +="4-Mas dias";
+let plazo = parseInt (prompt (textoPlazo)); 
+
+const fechaActual = new Date ();
+
+const dia = fechaActual.getDate();
+const mes = fechaActual.getMonth() + 1;
+const anio = fechaActual.getFullYear();
+
+const fechaContatenada = (dia + "/" + mes + "/" + anio);
+
+
+// let textoTasa = "Ingrese tipo de Tasa\n\n";
+//     textoTasa += "1= 30%\n";
+//     textoTasa += "2= 40%\n";
+//     textoTasa += "3= 50%\n";
+//     textoTasa += "4= 60%\n";
+
+// let Tasa = parseInt(prompt("Ingrese Tasa a cotizar segun plazo: "))    
+
+// Chequeando como llegan los datos ingresados
+    console.log("Cliente: " + nombreCliente);
+    console.log ("Tipo Cliene: " + cliente);
+    console.log ("Cliente" + tipoCliente);
+    // console.log ("Tipo Plazo Fijo:  "+ tipoPlazoFijo);
+    console.log("Monto: $" + monto);
+    console.log ("Plazo: " + plazo);
+    console.log (fechaActual);
+    console.log (dia);
+    console.log (mes);
+    console.log (anio);
+    console.log (fechaContatenada);
+
+    // PROCESAMIENTO DE DATOS
+    let plazoFijoPuro = calcularPlazoFijoPuro (monto, plazo);
+    let plazoFijoTNA = calcularTNA (plazo, plazoFijoPuro);
+    let plazoFijoCobrar = plazoFijoTNA;
+    let plazoFijodias= calcularFechaVto (plazo);
+    let fechaVto = plazoFijodias;
+
+    const diaVto = fechaVto.getDate();
+    const mesVto = fechaVto.getMonth() + 1;
+    const anioVto = fechaVto.getFullYear();
+    const fechaVtoContatenada = (diaVto + "/" + mesVto + "/" + anioVto);
+
+    console.log (fechaVtoContatenada);
+
+
+
+    // let fechaVto = calcularFechaVto (fechaActual, plazo);
+    // let fechaVtoplazofijo = fechaVto;
+
+
+    // SALIDA DE DATOS
+informarPlazoFijo(nombreCliente, cliente, monto, plazo, plazoFijoCobrar, fechaContatenada,fechaVtoContatenada);
+nombreCliente = prompt("Ingrese otro Nombre y Apellido: (ESC para salir)");
+}
+
+function calcularFechaVto(plazo) {
+    const fechaActual = new Date();
+    let diasASumar;
+
+    switch (plazo) {
+        case 1:
+            diasASumar = 30;
+            break;
+        case 2:
+            diasASumar = 60;
+            break;
+        case 3:
+            diasASumar = 90;
+            break;
+        
+        case 4:
+            diasASumar = 120;
+            break;
+        default:
+                return "Plazo no válido. Debes proporcionar 1, 2, 3 o 4.";
+    }
+
+    const fechaVto = sumarDias(fechaActual, diasASumar);
+    return fechaVto;
+}
+
+function sumarDias (fecha, plazo){
+    fecha.setDate (fecha.getDate () + plazo);
+    return fecha;
+}
+
+
+function calcularPlazoFijoPuro (monto, plazo){
+    return (monto * plazo); 
+}
+
+function calcularTNA (plazo, plazoFijoPuro){
+    let TNA;
+
+    if (plazo == 1) {
+        TNA = 30/12;
+    } else if (plazo == 2) {
+        TNA = 40/12;
+    } else if (plazo == 3){
+        TNA = 50/12;
+    } else{
+        TNA = 60/12;
+    }
+
+    return (plazoFijoPuro * TNA)/100;
+
+}
+
+function informarPlazoFijo (nombreCliente, cliente, monto, plazo, plazoFijoCobrar, fechaContatenada, fechaVtoContatenada) {
     let textoPlazo;
 
     if (plazo == 1) {
         textoPlazo = 30;
-    } else if (plazo == 2) {
+    } else if (plazo == 2){
         textoPlazo = 60;
-    } else if (plazo = 3) {
+    } else if (plazo ==3){
         textoPlazo = 90;
-    } else {
+    } else{
         textoPlazo = 120;
     }
 
-alert("Cliente: " + nombre + "\nMonto: $" + monto + "\nPlazo: "  + tasa);
+    alert("Cliente: " + nombreCliente + "\nTipo de Cliente: " + cliente + "\nMonto: $" + monto + "\nPlazo: " + textoPlazo + "\nIntereses ganados: " + plazoFijoCobrar.toFixed(2) 
+    + "\nFecha Constitucion: " + fechaContatenada  + "\nFecha vto: "+ fechaVtoContatenada );
 }
+
+
+
